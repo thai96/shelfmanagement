280a8c100c8ce23f6f6278fb68965e4ccce5118c
ThaiPQ <thaipqhe151275@fpt.edu.vn>
2026-06-01 21:07:17 +0700

    Init project

---
+++ src/main/java/com/thai/pham/shelfmanagement/ShelfmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.thai.pham.shelfmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thai/pham/shelfmanagement/ShelfmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.pham.shelfmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
