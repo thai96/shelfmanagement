b8c06cd94847d953392b1c7a5ec9648bbcfb0596
ThaiPQ <thaipqhe151275@fpt.edu.vn>
2026-06-07 16:57:14 +0700

    Add data module

---
+++ persistent_data/src/main/java/com/thai/pham/persistent_data/PersistentDataApplication.java
@@ -0,0 +1,13 @@
+package com.thai.pham.persistent_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentDataApplication.class, args);
+	}
+
+}
---
+++ persistent_data/src/test/java/com/thai/pham/persistent_data/PersistentDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.pham.persistent_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistentDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
