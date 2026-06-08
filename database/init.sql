05c9b4c0cca2d0afc33697354ce56c475404cfe0
ThaiPQ <thaipqhe151275@fpt.edu.vn>
2026-06-08 21:11:00 +0700

    Add temporary init.sql for initialize database

---
+++ database/init.sql
@@ -0,0 +1,10 @@
+\set ON_ERROR_STOP on
+CREATE DATABASE inventory_db
+WITH ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8';
+
+CREATE DATABASE ledger_db
+WITH ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8';
