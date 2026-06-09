ebb77e368276b5e67a7edc801f5d87a627ef93bf
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2026-06-09 18:44:02 +0700

    Add product attributes and converter

---
+++ storage_routing_service/src/main/entity/ProductAttribute.java
@@ -0,0 +1,30 @@
+package thai.pham.storageroutingservice.entity;
+
+@Data
+class ProductAttributes {
+    private String color;
+    private Size size;
+
+    public static final class ProductAttributesConverter implements AttributeConverter<ProductAttributes, String> {
+        private final ObjectMapper mapper = new ObjectMapper();
+        
+        @Override
+        public String convertToDatabaseColumn(ProductAttributes productAttribute) {
+            try {
+                return productAttribute == null ? null : objectMapper.writeValueAsString(productAttribute);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException("Error converting object to JSON string", e);
+            }
+        }
+
+
+        @Override
+        public MyCustomObject convertToEntityAttribute(String dbData) {
+            try {
+                return dbData == null ? null : objectMapper.readValue(dbData, ProductAttributes.class);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException("Error converting JSON string to object", e);
+            }
+        }
+    }
+}
