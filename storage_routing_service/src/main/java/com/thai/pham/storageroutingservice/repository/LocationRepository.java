9900c5cd6385e58b9f8fc47a11f5bac9f8175519
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2026-06-11 17:07:13 +0700

    Add method for location service & repository

--- storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/repository/LocationRepository.java
+++ storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/repository/LocationRepository.java
@@ -1,4 +1,8 @@
 package thai.pham.storageroutingservice.repository;
 
 @ReadOnlyRepository
-public interface LocationRepository extends JpaRepository<Location, UUID> {}
+public interface LocationRepository extends JpaRepository<Location, UUID> {
+    public Page<Location> findLocationByLocationType(LocationType locationType, Pageable pageable);
+
+    public Page<Location> findLocationByNameContaining(String name, Pageable pageable);
+}
--- storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/service/LocationService.java
+++ storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/service/LocationService.java
@@ -10,6 +10,18 @@ public class LocationService {
     }
 
     public List<Location> getAllLocation() {
-        return locationRepo.getAll();
+        return locationRepo.findAll();
+    }
+
+    public Page<Location> getWareHouseLocation(Pageable pageable){
+        return locationRepo.findLocationByLocationType(LocationType.WARE_HOUSE);
+    }
+
+    public Page<Location> getStoreLocation(Pageable pageable){
+        return locationRepo.findLocationByLocationType(LocationType.STORE);
+    }
+
+    public Page<Location> getLocationByName(String input, Pageable pageable) {
+        return locationRepo.findLocationByNameContaining(input, pageable);
     }
 }
