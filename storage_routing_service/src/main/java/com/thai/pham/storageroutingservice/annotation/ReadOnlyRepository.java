f8df353bb0027bb73e87ce78c30f531b83d331f3
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2026-06-11 12:08:25 +0700

    Change the manual request routing to transactional routing using spring framework

--- storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/annotation/ReadOnlyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thai.pham.storageroutingservice.annotation;
-
-@Retention(RetentionPolicy.RUNTIME) // Used so that spring can processing in runtime
-@Target({ElementType.TYPE}) // Ensure the annotation only used in desired location - in this case only for class, interface or enums
-@Documented // Help to include the annotation in javadoc where annotation is used
-public @interface ReadOnlyRepository {
-}
---
+++ storage_routing_service/src/main/java/com/thai/pham/storageroutingservice/configs/RequestRouting.java
@@ -0,0 +1,21 @@
+package com.thai.pham.storageroutingservice.configs;
+
+public class ReadRoutingDataSource extends AbstractRoutingDataSource {
+    private final AtomicInteger counter = new AtomicInteger(0);
+    private final int slaveCount = 5;
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        // If @Transactional(readOnly = true), route to a slave
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            int index = Math.abs(counter.getAndIncrement() % slaveCount) + 1;
+            String slaveKey = "slave" + index;
+            System.out.println("Routing to -> " + slaveKey);
+            return slaveKey;
+        }
+        
+        // Otherwise, route to Master (Writes)
+        System.out.println("Routing to -> master");
+        return "master";
+    }
+}
