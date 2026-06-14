3a9c4222351599c498bfcf85edeb188c306638fa
ThaiPQ <thaipqhe151275@fpt.edu.vn>
2026-06-14 21:37:15 +0700

    Add Redis default configuration

---
+++ inventoryservice/src/main/java/com/thai/pham/inventoryservice/configs/RedisCacheConfig.java
@@ -0,0 +1,26 @@
+package com.thai.pham.inventoryservice.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheConfiguration(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<Object>(Object.class)));
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfiguration).build();
+    }
+}
