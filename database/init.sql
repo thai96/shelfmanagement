d0399bdd595f018348a7f8849e54b72e68824443
thai_pham <thaiphamquang99@gmail.com>
2026-07-05 21:42:43 +0700

    Fix read only user cannot connect

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,10 @@
 \set ON_ERROR_STOP on
-CREATE DATABASE inventory_db
-WITH ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8';
+SET synchronous_commit = local;
+CREATE USER myuser_readonly WITH PASSWORD 'strong_password';
+GRANT CONNECT ON DATABASE inventory_db TO myuser_readonly;
+GRANT USAGE ON SCHEMA public TO myuser_readonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO myuser_readonly;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO myuser_readonly;
+ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON TABLES TO myuser_readonly;
+ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON SEQUENCES TO myuser_readonly;
 
-CREATE DATABASE ledger_db
-WITH ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8';
