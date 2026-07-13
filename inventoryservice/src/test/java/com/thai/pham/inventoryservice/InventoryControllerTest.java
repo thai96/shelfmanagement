78927af1628aab02c697420c8629e1562c004605
thai_pham <thaiphamquang99@gmail.com>
2026-07-13 21:43:03 +0700

    Update the test logic of testGetInventoryPage

--- inventoryservice/src/test/java/com/thai/pham/inventoryservice/InventoryControllerTest.java
+++ inventoryservice/src/test/java/com/thai/pham/inventoryservice/InventoryControllerTest.java
@@ -1,27 +1,64 @@
-package com.thai.pham.persistent_data;
+package com.thai.pham.inventoryservice;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.thai.pham.inventoryservice.dto.InventoryDto;
+import com.thai.pham.inventoryservice.dto.PageDto;
+import com.thai.pham.inventoryservice.entity.Inventory;
+import com.thai.pham.inventoryservice.repository.InventoryRepository;
+import com.thai.pham.inventoryservice.service.InventoryService;
 import com.thai.pham.inventoryservice.controller.InventoryController;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Map;
+import java.util.stream.Stream;
 
 @WebMvcTest(InventoryController.class)
 public class InventoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
+    private InventoryRepository inventoryRepository;
+
+    @InjectMocks
     private InventoryService inventoryService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @ParameterizedTest
-    public void testGetInventoryPage(PageDto<InventoryDto> expectedOutput, Map<String, String> pageData) {
-        mockMvc.perform(get("api/v1/inventory/"));
-        pageData.forEach((paramsName, paramsData) -> {
-            mockMvc.param(paramsName, paramsData);
-        });
-        mockMvc.contentType(ContentType.APPLICATION_JSON)
-        .andExpect(status().isOk());
+    @MethodSource("getInventoryPageData")
+    public void testGetInventoryPage(PageDto<InventoryDto> expectedOutput, Page<Inventory> outputOfRepo, Map<String, String> pageData) throws Exception {
+        when(inventoryRepository.findAll(any(Pageable.class))).thenReturn(outputOfRepo);
+
+        String expectedDataAsJson = objectMapper.writeValueAsString(expectedOutput);
+        MockHttpServletRequestBuilder requestBuilder = get("api/v1/inventory/");
+        pageData.forEach(requestBuilder::param);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedDataAsJson));
     }
 
-    
+    public static Stream<Arguments> getInventoryPageData() {
+        return Stream.of(
+
+        );
+    }
 }
