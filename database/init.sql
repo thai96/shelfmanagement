78b7ae06b843edc277c08f2b68c077f46e685c0a
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2026-07-13 13:37:33 +0700

    Moev permisison to role instead of user

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,12 @@
 \set ON_ERROR_STOP on
 SET synchronous_commit = local;
-CREATE USER myuser_readonly WITH PASSWORD 'strong_password';
-GRANT CONNECT ON DATABASE inventory_db TO myuser_readonly;
-GRANT USAGE ON SCHEMA public TO myuser_readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO myuser_readonly;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO myuser_readonly;
-ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON TABLES TO myuser_readonly;
-ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON SEQUENCES TO myuser_readonly;
+CREATE ROLE inventory_readonly;
+GRANT CONNECT ON DATABASE inventory_db TO inventory_readonly;
+GRANT USAGE ON SCHEMA public TO inventory_readonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO inventory_readonly;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO inventory_readonly;
+ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON TABLES TO inventory_readonly;
+ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON SEQUENCES TO inventory_readonly;
 
+CREATE USER myuser_readonly WITH PASSWORD 'strong_password';
+GRANT inventory_readonly TO myuser_readonly;
