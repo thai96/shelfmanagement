7fa2ff4fd34708c539e5583c2209860a85133009
thai_pham <thaiphamquang99@gmail.com>
2026-07-14 21:33:46 +0700

    Fix build failed on unittest on inventorycontroller

--- inventoryservice/src/test/java/com/thai/pham/inventoryservice/InventoryControllerTest.java
+++ inventoryservice/src/test/java/com/thai/pham/inventoryservice/InventoryControllerTest.java
@@ -1,25 +1,34 @@
 package com.thai.pham.inventoryservice;
 
+import com.thai.pham.inventoryservice.configs.DataSourceConfig;
+import com.thai.pham.inventoryservice.configs.RedisCacheConfig;
 import com.thai.pham.inventoryservice.dto.InventoryDto;
 import com.thai.pham.inventoryservice.dto.PageDto;
-import com.thai.pham.inventoryservice.entity.Inventory;
-import com.thai.pham.inventoryservice.repository.InventoryRepository;
+import com.thai.pham.inventoryservice.entity.*;
+import com.thai.pham.inventoryservice.mapper.InventoryDtoMapper;
+import com.thai.pham.inventoryservice.mapper.LocationDtoMapper;
+import com.thai.pham.inventoryservice.mapper.ProductDtoMapper;
 import com.thai.pham.inventoryservice.service.InventoryService;
 import com.thai.pham.inventoryservice.controller.InventoryController;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.data.domain.Page;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,38 +36,63 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Stream;
 
-@WebMvcTest(InventoryController.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(
+        controllers = InventoryController.class,
+        excludeAutoConfiguration = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class,
+                DataSourceTransactionManagerAutoConfiguration.class
+        },
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = {
+                                DataSourceConfig.class,
+                                RedisCacheConfig.class
+                        }
+                )
+        }
+)
 public class InventoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private InventoryRepository inventoryRepository;
-
-    @InjectMocks
+    @MockitoBean
     private InventoryService inventoryService;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private JsonMapper jsonMapper;
+
+    private static InventoryDtoMapper mapper = new InventoryDtoMapper(new ProductDtoMapper(), new LocationDtoMapper());
 
     @ParameterizedTest
     @MethodSource("getInventoryPageData")
-    public void testGetInventoryPage(PageDto<InventoryDto> expectedOutput, Page<Inventory> outputOfRepo, Map<String, String> pageData) throws Exception {
-        when(inventoryRepository.findAll(any(Pageable.class))).thenReturn(outputOfRepo);
+    public void testGetInventoryPage(PageDto<InventoryDto> expectedOutput, Map<String, String> pageData) throws Exception {
+        when(inventoryService.getAllInventory(any(Pageable.class))).thenReturn(expectedOutput);
 
-        String expectedDataAsJson = objectMapper.writeValueAsString(expectedOutput);
-        MockHttpServletRequestBuilder requestBuilder = get("api/v1/inventory/");
+        String expectedDataAsJson = jsonMapper.writeValueAsString(expectedOutput);
+        MockHttpServletRequestBuilder requestBuilder = get("/api/v1/inventory/");
         pageData.forEach(requestBuilder::param);
         mockMvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(expectedDataAsJson));
     }
 
     public static Stream<Arguments> getInventoryPageData() {
+        Map<String, String> firstPageData = Map.of("page", "0", "size", "3");
+        List<Inventory> expectedReturnInventory = List.of(
+                new Inventory(UUID.randomUUID(), 12, 10, 2, new Location(UUID.randomUUID(),"Warehouse 1",  LocationType.WARE_HOUSE, true), new Product(UUID.randomUUID(), "sku", "product name", new ProductAttributes("color", "size"))),
+                new Inventory(UUID.randomUUID(), 109, 29, 99, new Location(UUID.randomUUID(),"Store 1",  LocationType.STORE, true), new Product(UUID.randomUUID(), "sku", "product name", new ProductAttributes("color", "size"))),
+                new Inventory(UUID.randomUUID(), 107, 7, 100, new Location(UUID.randomUUID(),"Warehouse 2",  LocationType.WARE_HOUSE, true), new Product(UUID.randomUUID(), "sku", "product name", new ProductAttributes("color", "size")))
+        );
         return Stream.of(
-
+            Arguments.of(new PageDto<>(expectedReturnInventory.stream().map(mapper::mapObject).toList(), 0, 10, 4L, 1L, true, true),
+                    firstPageData)
         );
     }
 }
