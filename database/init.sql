d519e07ff9bc67acbe3eadb8f454183b34caadb1
thai_pham <thaiphamquang99@gmail.com>
2026-07-19 22:16:06 +0700

    Fix master db cannot run when restart image Update init script to not create role when is is existed

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,33 @@
 \set ON_ERROR_STOP on
 SET synchronous_commit = local;
-CREATE ROLE inventory_readonly;
-GRANT CONNECT ON DATABASE inventory_db TO inventory_readonly;
-GRANT USAGE ON SCHEMA public TO inventory_readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO inventory_readonly;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO inventory_readonly;
-ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON TABLES TO inventory_readonly;
-ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON SEQUENCES TO inventory_readonly;
 
-CREATE USER myuser_readonly WITH PASSWORD 'strong_password';
-GRANT inventory_readonly TO myuser_readonly;
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE rolname = 'inventory_readonly'
+   ) THEN
+        CREATE ROLE inventory_readonly;
+        GRANT CONNECT ON DATABASE inventory_db TO inventory_readonly;
+        GRANT USAGE ON SCHEMA public TO inventory_readonly;
+        GRANT SELECT ON ALL TABLES IN SCHEMA public TO inventory_readonly;
+        GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO inventory_readonly;
+        ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON TABLES TO inventory_readonly;
+        ALTER DEFAULT PRIVILEGES FOR ROLE myuser IN SCHEMA public GRANT SELECT ON SEQUENCES TO inventory_readonly;
+    END IF;
+
+    IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE rolname = 'myuser_readonly'
+   ) THEN
+    CREATE USER myuser_readonly WITH PASSWORD 'strong_password';
+    GRANT inventory_readonly TO myuser_readonly;
+    END IF;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'Error Code: %', SQLSTATE;
+        RAISE NOTICE 'Error Message: %', SQLERRM;
+END
+$$
