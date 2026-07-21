f3510127631ac1df3be6ceaa9625c1291b8e49db
thai_pham <thaiphamquang99@gmail.com>
2026-07-21 22:02:30 +0700

    Change the Application name to correct service name :StorageRoutingApplication

--- inventoryservice/src/main/java/com/thai/pham/inventoryservice/InventoryServiceApplication.java
+++ inventoryservice/src/main/java/com/thai/pham/inventoryservice/InventoryServiceApplication.java
@@ -2,11 +2,10 @@ package com.thai.pham.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class StorageRoutingApplication {
+public class InventoryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StorageRoutingApplication.class, args);
+        SpringApplication.run(InventoryServiceApplication.class, args);
     }
 }
